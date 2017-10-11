e3ddf2ae613aceeed322739ad30beedc13bbc265
Huimee Sanchez <sanchezh95@gmail.com>
2017-10-11 10:35:46 -0700

    adding second tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -7,6 +7,8 @@ import { HomePage } from '../home/home';
 import { BucketsPage } from '../buckets/buckets';
 import { NotificationsPage } from '../notifications/notifications';
 import { LogoutPage } from '../logout/logout';
+import { BillsPage } from '../bills/bills';
+import { SavingsPage } from '../savings/savings';
 
 /**
  * Generated class for the TabsPage tabs.
@@ -24,9 +26,12 @@ export class TabsPage {
 
   tab1 = NotificationsPage;
   tab2 = AccountsPage ;
-  tab3 = BucketsPage;
+  tab3 = HomePage;
   tab4 = OverviewPage;
   tab5 = LogoutPage;
+  tab6 = BillsPage;
+  tab7 = HomePage;
+  tab8 = SavingsPage;
 
   constructor(public navCtrl: NavController) {
 
